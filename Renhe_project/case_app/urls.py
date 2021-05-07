ece8d310df1dd3f3310ab6a574bda0aac90b66c1
Ji-Yang Lin <xuunnis123@gmail.com>
2021-05-07 13:02:20 +0800

    Add Case Filter and consider design pattern

--- Renhe_project/case_app/urls.py
+++ Renhe_project/case_app/urls.py
@@ -1,8 +1,10 @@
 from case_app import views
 from django.urls import path
+from django_filters.views import FilterView
+from .filters import CaseFilter
 app_name = 'case_app'
 urlpatterns=[
-    path('', views.CaseListView.as_view(),name='list'),
+    path('', FilterView.as_view(filterset_class=CaseFilter,template_name='case_app/case_list.html'),name='list'),
     path('<int:pk>/', views.CaseDetailView.as_view(), name='detail'),
     path('create/',views.CaseCreateView.as_view(), name='create'),
     path('update/<int:pk>/', views.CaseUpdateView.as_view(), name='update'),
